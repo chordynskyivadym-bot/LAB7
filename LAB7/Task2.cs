e9301c896277aa9a989c36d4717e8bd52aadae7a
user <user@DESKTOP-5SD5AN8>
2026-02-15 22:13:05 +0200

    Two part of the TASK2

--- LAB7/Task2.cs
+++ LAB7/Task2.cs
@@ -3,11 +3,11 @@ namespace LAB7
 {
     public class BankTerminal
     {
-        public Action<int> OnMoneyWithdraw;
+        public event Action<int> OnMoneyWithdraw;
 
         public void Withdraw(int amount)
         {
-            Console.WriteLine($"[Термінал] Видаємо {amount} грн...");
+            Console.WriteLine($"Видаємо {amount} грн");
 
             OnMoneyWithdraw?.Invoke(amount);
         }
@@ -19,18 +19,17 @@ namespace LAB7
         {
             BankTerminal terminal = new BankTerminal();
 
-            terminal.OnMoneyWithdraw += (sum) => Console.WriteLine($"> SMS: Знято {sum} грн.");
+            terminal.OnMoneyWithdraw += (sum) => Console.WriteLine($"SMS: Знято {sum} грн.");
 
             terminal.Withdraw(500);
 
             Console.WriteLine("\nПОЧАТОК ЗЛОМУ");
 
-            terminal.OnMoneyWithdraw.Invoke(999999);
+            //terminal.OnMoneyWithdraw.Invoke(999999);
 
-            terminal.OnMoneyWithdraw = null;
+            //terminal.OnMoneyWithdraw = null;
 
-            Console.WriteLine("Намагаємось зняти гроші після злому:");
-            terminal.Withdraw(200);
+            Console.WriteLine("Не можливо зламати термінал. Вибиває помилку");
         }
     }
 }
