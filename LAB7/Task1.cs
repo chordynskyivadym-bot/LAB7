153b04d97b4f5a2117cf814cf293897437e1ff79
user <user@DESKTOP-5SD5AN8>
2026-02-15 22:06:06 +0200

    one part of the TASK2

---
+++ LAB7/Task1.cs
@@ -0,0 +1,21 @@
+﻿namespace LAB7
+{
+    internal class Task1
+    {
+        //static void Main(string[] args)
+        //{
+        //    List<Action> action = new List<Action>();
+
+        //    for(int i = 1; i <= 5; i++)
+        //    {
+        //        int index = i;
+        //        action.Add(() => Console.WriteLine(index));
+        //    }
+
+        //    foreach (var item in action)
+        //    {
+        //        item();
+        //    }
+        //}
+    }
+}
---
+++ LAB7/Task2.cs
@@ -0,0 +1,36 @@
+﻿
+namespace LAB7
+{
+    public class BankTerminal
+    {
+        public Action<int> OnMoneyWithdraw;
+
+        public void Withdraw(int amount)
+        {
+            Console.WriteLine($"[Термінал] Видаємо {amount} грн...");
+
+            OnMoneyWithdraw?.Invoke(amount);
+        }
+    }
+
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            BankTerminal terminal = new BankTerminal();
+
+            terminal.OnMoneyWithdraw += (sum) => Console.WriteLine($"> SMS: Знято {sum} грн.");
+
+            terminal.Withdraw(500);
+
+            Console.WriteLine("\nПОЧАТОК ЗЛОМУ");
+
+            terminal.OnMoneyWithdraw.Invoke(999999);
+
+            terminal.OnMoneyWithdraw = null;
+
+            Console.WriteLine("Намагаємось зняти гроші після злому:");
+            terminal.Withdraw(200);
+        }
+    }
+}
